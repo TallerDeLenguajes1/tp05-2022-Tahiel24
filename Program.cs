4c123ade779aadfd2a67fe3e085e68c214738b49
Tahiel24 <tahielvarela@gmail.com>
2022-05-18 16:50:51 -0300

    Punto 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mundo!");
-Console.WriteLine("Esto es una funcion");
+﻿//Ingresa un numero
+Console.WriteLine("Ingrese un numero: ");
+string original = Console.ReadLine();
+
+//Convierte el texto en un dato tipo char[]
+char[] numArray = original.ToCharArray();
+
+string reverse = String.Empty;
+//Da vuelta la cadena
+for (int i = numArray.Length - 1; i >= 0; i--)
+{
+    reverse += numArray[i];
+}
+//Muestra
+Console.WriteLine("Numero original: " + original);
+Console.WriteLine("Numero invertido: " + reverse);
